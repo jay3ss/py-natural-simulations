3e264d90faeb1fa9b3ef580c416e18c2f4573fde
jay ess <mentoce@gmail.com>
2020-10-27 22:11:25 -0700

    Create 'monte_carlo' function

--- helpers/mathematics.py
+++ helpers/mathematics.py
@@ -1,7 +1,20 @@
 """Helpers"""
 import math
+import random
 
 
 def normal_pdf(x, mu=0.0, sigma=1.0):
     x = (x - mu) / sigma
     return math.exp(-x*x / 2.) / sigma / math.sqrt(2 * math.pi)
+
+
+def monte_carlo():
+    """Generate random numbers, but prefer higher numbers."""
+
+    while True:
+        rand1 = random.random()
+        rand2 = random.random()
+        probability = rand1
+
+        if rand2 < probability:
+            return rand1
