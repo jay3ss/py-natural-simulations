1f2006f3138ba84ce15311235a8f0c956dfb457d
jay ess <mentoce@gmail.com>
2020-10-27 22:24:43 -0700

    Create test function for different uses of MC method

--- helpers/mathematics.py
+++ helpers/mathematics.py
@@ -8,13 +8,16 @@ def normal_pdf(x, mu=0.0, sigma=1.0):
     return math.exp(-x*x / 2.) / sigma / math.sqrt(2 * math.pi)
 
 
-def monte_carlo():
+def monte_carlo(test_func=None):
     """Generate random numbers, but prefer higher numbers."""
+
+    if test_func is None:
+        test_func = lambda a, b: a < b
 
     while True:
         rand1 = random.random()
         rand2 = random.random()
         probability = rand1
 
-        if rand2 < probability:
+        if test_func(rand2, probability):
             return rand1
