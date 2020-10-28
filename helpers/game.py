cbfab3df55bc3c2940579fb8ced2e87eabb7f768
jay ess <mentoce@gmail.com>
2020-10-27 22:33:14 -0700

    Add function to determine if position is within the bounds of the window

--- helpers/game.py
+++ helpers/game.py
@@ -7,3 +7,11 @@ def init_game(width, height):
     screen = pygame.display.set_mode([width, height])
 
     return screen
+
+
+def is_in_bounds(position, bounds):
+    x, y = position
+    w, h = bounds
+
+    return 0 <= x  <= w or 0 <= y <= h
+
